2d29d5422201eca9feaaaca07b8df0cf41cb66bb
ardan <francesco.dalri-2@unitn.it>
2026-05-29 11:35:29 +0200

    examples synth + OSC

---
+++ live_inference/mock_osc_sender.py
@@ -0,0 +1,17 @@
+# ESEMPIO: 
+# manda 3 messaggi OSCtemporizzati a SuperCollider per controllare il synth. 
+
+
+from pythonosc.udp_client import SimpleUDPClient
+import time
+
+SC_IP = "127.0.0.1"
+SC_PORT = 57120
+
+client = SimpleUDPClient(SC_IP, SC_PORT)
+
+client.send_message("/synth/control", [440, 0.2])
+time.sleep(1)
+client.send_message("/synth/control", [660, 0.15])
+time.sleep(1)
+client.send_message("/synth/control", [220, 0.4])
